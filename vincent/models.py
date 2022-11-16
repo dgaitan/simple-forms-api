632c6a495526f79478839da84ded255d898f8808
David Gaitan <jdavid.gaitan@gmail.com>
2022-11-15 21:58:05 -0600

    Adding BaseModal class with base fields

--- vincent/models.py
+++ vincent/models.py
@@ -2,7 +2,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
-class User(AbstractUser):
+
+class BaseModel(models.Model):
+    """Primary Model Base
+    
+    Add here common fields related
+    """
+    created_at = models.DateTimeField(
+        _('Created At'), 
+        auto_now_add=True, 
+        blank=True, 
+        null=False
+    )
+    updated_at = models.DateTimeField(
+        _('Updated At'),
+        auto_now=True,
+        blank=True,
+        null=False
+    )
+
+    class Meta:
+        abstract = True
+
+
+class User(AbstractUser, BaseModel):
     """Default User"""
 
     def __str__(self):
