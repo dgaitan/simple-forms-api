c087f1ada7db0f0e5e66403d0f1ceab3f5176230
David Gaitan <jdavid.gaitan@gmail.com>
2022-11-15 22:11:56 -0600

    Create initial Form Model

--- vincent/models.py
+++ vincent/models.py
@@ -30,3 +30,43 @@ class User(AbstractUser, BaseModel):
 
     def __str__(self):
         return self.username
+
+
+class Form(BaseModel):
+    """Form Model"""
+
+    class Statuses(models.IntegerChoices):
+        DRAFT = 1, _('Draft'),
+        PUBLISHED = 2, _('Published'),
+        TRASHED = 3, _('Trashed')
+
+    name = models.CharField(
+        _('Name'),
+        max_length=255,
+        help_text=_('Define a Name to your form')
+    )
+    description = models.TextField(
+        _('Description'),
+        blank=True,
+        null=True,
+        help_text=_('Give a description to your form if necessary')
+    )
+    status = models.IntegerField(
+        _('Status'),
+        blank=True,
+        default=Statuses.DRAFT,
+        choices=Statuses.choices
+    )
+    created_by = models.ForeignKey(
+        User,
+        verbose_name=_('Created By'),
+        on_delete=models.CASCADE,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = _('Form')
+        verbose_name_plural = _('Forms')
+
+    def __str__(self):
+        return self.name
