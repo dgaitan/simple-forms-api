59e46c1ebe01351de85f2a134b1b085c85c41639
David Gaitan <jdavid.gaitan@gmail.com>
2022-11-16 21:23:57 -0600

    adding form field tests

--- vincent/tests/test_models.py
+++ vincent/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from vincent.models import User, Form
+from vincent.models import User, Form, FormField
 
 class UserModelTest(TestCase):
 
@@ -81,3 +81,62 @@ class FormModelTest(TestCase):
 
         self.assertEqual(Form.Statuses.TRASHED, form.status)
         self.assertEqual(3, form.status)
+
+
+class FormFieldsTest(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(
+            username='dgaitan',
+            password='secret'
+        )
+        self.form = Form.objects.create(
+            name='My New Form',
+            created_by=self.user,
+            status=Form.Statuses.PUBLISHED
+        )
+
+    def test_adding_a_field_to_form(self) -> None:
+        field = self.form.fields.create(label='Name')
+
+        self.assertEqual('Name', field.label)
+        self.assertEqual('name', field.name)
+        self.assertEqual(FormField.FieldTypes.TEXT, field.field_type)
+        self.assertFalse(field.required)
+        self.assertIsNone(field.placeholder)
+        self.assertEqual('My New Form', field.form.name)
+
+    def test_adding_a_field_with_all_values_defined(self) -> None:
+        field = self.form.fields.create(
+            label='First Name',
+            placeholder='John Doe',
+            required=True,
+            field_type=FormField.FieldTypes.TEXT
+        )
+
+        self.assertEqual('First Name', field.label)
+        self.assertEqual('first_name', field.name)
+        self.assertEqual(FormField.FieldTypes.TEXT, field.field_type)
+        self.assertTrue(field.required)
+        self.assertIsNotNone(field.placeholder)
+        self.assertEqual('John Doe', field.placeholder)
+        self.assertEqual('My New Form', field.form.name)
+
+    def test_adding_various_fields_to_form(self) -> None:
+        self.form.fields.create(label='First Name', required=True)
+        self.form.fields.create(label='Email', required=True, field_type=FormField.FieldTypes.EMAIL)
+        self.form.fields.create(label='Message', required=True, field_type=FormField.FieldTypes.TEXTAREA)
+
+        self.assertEqual(3, self.form.fields.count())
+
+    def test_adding_field_with_options(self) -> None:
+        select = self.form.fields.create(
+            label='Programming Language',
+            field_type=FormField.FieldTypes.SELECT
+        )
+
+        select.options.create(name='Python', order=1)
+        select.options.create(name='PHP', order=2)
+        select.options.create(name='JavaScript', order=3)
+
+        self.assertEqual(3, select.options.count())
