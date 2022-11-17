1371df8390ca2a1647729100fac23acda7e89062
David Gaitan <jdavid.gaitan@gmail.com>
2022-11-16 20:37:10 -0600

    adding form model tests

--- vincent/tests/test_models.py
+++ vincent/tests/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from vincent.models import User
+from vincent.models import User, Form
 
 class UserModelTest(TestCase):
 
@@ -32,3 +32,52 @@ class UserModelTest(TestCase):
         self.assertEqual('David Gaitan', user.get_full_name())
         self.assertFalse(user.is_staff)
         self.assertTrue(user.is_active)
+
+
+class FormModelTest(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(
+            username='dgaitan',
+            password='secret'
+        )
+
+    def test_creating_only_form(self) -> None:
+        form = Form.objects.create(
+            name='My New Form',
+            created_by=self.user,
+            status=Form.Statuses.PUBLISHED
+        )
+
+        self.assertEqual('My New Form', form.name)
+        self.assertEqual(Form.Statuses.PUBLISHED, form.status)
+        self.assertEqual(2, form.status)
+        self.assertIsInstance(form.created_by, User)
+        self.assertEqual('dgaitan', form.created_by.username)
+
+    def test_creating_draft_form(self) -> None:
+        form = Form.objects.create(
+            name='My New Draft Form',
+            created_by=self.user,
+        )
+
+        self.assertEqual('My New Draft Form', form.name)
+        self.assertEqual(Form.Statuses.DRAFT, form.status)
+        self.assertEqual(1, form.status)
+
+    def test_trashing_a_form(self) -> None:
+        form = Form.objects.create(
+            name='My New Form',
+            created_by=self.user,
+            status=Form.Statuses.PUBLISHED
+        )
+
+        self.assertEqual('My New Form', form.name)
+        self.assertEqual(Form.Statuses.PUBLISHED, form.status)
+        self.assertEqual(2, form.status)
+
+        form.status = Form.Statuses.TRASHED
+        form.save()
+
+        self.assertEqual(Form.Statuses.TRASHED, form.status)
+        self.assertEqual(3, form.status)
